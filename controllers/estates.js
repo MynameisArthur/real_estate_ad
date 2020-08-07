300a5b3d26d6ff7f921f6f07d0dcf3a62d070a52
Arthur <arthurd800@wp.pl>
2020-08-07 13:43:18 +0200

    added pagination

--- controllers/estates.js
+++ controllers/estates.js
@@ -12,7 +12,7 @@ exports.getEstates = asyncHandler(async (req, res, next) => {
     // Copy req.query
     const reqQuery = {...req.query};
     //Fields to execute
-    const removeFields = ['select', 'sort'];
+    const removeFields = ['select', 'sort', 'page', 'limit'];
     //Loop over removeFields and delete them from reqQuery
     removeFields.forEach((param) => delete reqQuery[param]);
 
@@ -37,12 +37,36 @@ exports.getEstates = asyncHandler(async (req, res, next) => {
     } else {
         query = query.sort('-createdAt');
     }
+    //Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Estate.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
     //Executing query
     const estates = await query;
+    //Pagination result
+    const pagination = {};
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit,
+        };
+    }
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit,
+        };
+    }
+
     res.status(200).json({
         success: true,
         data: estates,
         count: estates.length,
+        pagination,
     });
 });
 
