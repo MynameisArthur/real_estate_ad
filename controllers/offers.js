2062515c10b4cda7a82b8477489c712af84c65d3
Arthur <arthurd800@wp.pl>
2020-08-09 12:12:51 +0200

    fixed highestBid value in estate document on updating or deleting an offer

--- controllers/offers.js
+++ controllers/offers.js
@@ -95,12 +95,25 @@ exports.addOffer = asyncHandler(async (req, res, next) => {
 
 exports.updateOffer = asyncHandler(async (req, res, next) => {
     let offer = await Offer.findById(req.params.id);
-
+    const estate = await Estate.findById(offer.estate._id);
     if (!offer) {
         return next(
             new ErrorResponse(`No course with the id of ${req.params.id}`),
             404
         );
+    }
+    //Check if money offered is at least as much as startingPrice
+    if (req.body.amountOffered < estate.startingPrice) {
+        return next(
+            new ErrorResponse(
+                `You need to offer at least $${estate.startingPrice}`
+            ),
+            400
+        );
+    }
+    //If more money is offered than current highestBid, update highestBid field in estate
+    if (req.body.amountOffered >= estate.highestBid) {
+        await estate.update({highestBid: req.body.amountOffered});
     }
     offer = await Offer.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
@@ -118,14 +131,33 @@ exports.updateOffer = asyncHandler(async (req, res, next) => {
 
 exports.deleteOffer = asyncHandler(async (req, res, next) => {
     const offer = await Offer.findById(req.params.id);
-
     if (!offer) {
         return next(
-            new ErrorResponse(`No course with the id of ${req.params.id}`),
+            new ErrorResponse(`No offer with the id of ${req.params.id}`),
             404
         );
     }
+
+    // get estate associated with offer
+    const estate = await Estate.findById(offer.estate);
+    //remove offer
     await offer.remove();
+    //find all offers for given id
+    const offers = await Offer.find({
+        estate: estate._id,
+    });
+    //extract highest amountOffered
+    let maxBid = Math.max(...offers.map((offer) => offer.amountOffered));
+    //if maxBid is less than 0 set it to startingPrice of the estate
+    if (maxBid < 0) {
+        maxBid = estate.startingPrice;
+    }
+
+    //update highestBid
+    await estate.update({
+        highestBid: maxBid,
+    });
+
     res.status(200).json({
         success: true,
         data: {},
