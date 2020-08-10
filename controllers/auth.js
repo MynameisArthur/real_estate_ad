a6d1f9cbd9b3b32ba439830a6519b2cdd59e7b85
Arthur <arthurd800@wp.pl>
2020-08-10 17:06:18 +0200

    added user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/User');
 
 // @desc  Register user
-// @route GET /real_estate_ad/auth/register
+// @route POST /real_estate_ad/auth/register
 // Public
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -15,7 +15,43 @@ exports.register = asyncHandler(async (req, res, next) => {
         password,
         role,
     });
+    //Create token
+    const token = user.getSignedJwtToken();
+
     res.status(200).json({
         success: true,
+        token,
+    });
+});
+
+// @desc  Login user
+// @route POST /real_estate_ad/auth/login
+// Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+    const {email, password} = req.body;
+    //Validate email and password
+    if (!email || !password) {
+        return next(
+            new ErrorResponse('Please provide an email and password', 400)
+        );
+    }
+    //Check for user
+    const user = await User.findOne({email}).select('+password');
+    if (!user) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+    //Check if password matches
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    //Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true,
+        token,
     });
 });
