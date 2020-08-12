34e43252680b30888fad4f0a0cba9178d932e15e
Arthur <arthurd800@wp.pl>
2020-08-12 14:41:11 +0200

    added update logged in user password functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -141,6 +141,25 @@ exports.updateDetails = asyncHandler(async (req, res, next) => {
     });
 });
 
+// @desc Update password
+// @route PUT /real_estate_ad/auth/updatepassword
+// @access Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+    //Check current password
+    if (!(await user.matchPassword(req.body.currentPassword))) {
+        return next(new ErrorResponse('Password is incorrect', 401));
+    }
+    user.password = req.body.newPassword;
+    await user.save();
+    sendTokenResponse(user, 200, res);
+
+    res.status(200).json({
+        success: true,
+        data: user,
+    });
+});
+
 //get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
     //Create token
