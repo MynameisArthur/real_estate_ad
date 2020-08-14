c4d78de804e25c31ee1aeb8e8c968b557c1e54c0
Arthur <arthurd800@wp.pl>
2020-08-14 13:22:13 +0200

    creating frontend part of application - added client folder; installed frontend dependencies, modified scripts in package.json

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+    return (
+        <div className='App'>
+            <h1>Real Estate AD</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+                ad molestiae rerum quis exercitationem commodi?
+            </p>
+        </div>
+    );
+}
+
+export default App;
